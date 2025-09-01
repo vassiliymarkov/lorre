17070d8e9d524de3bbc610df563404f8796b6e3c
vassiliymarkov <88937545+vassiliymarkov@users.noreply.github.com>
2025-09-01 07:51:48 +0200

    Add files via upload

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,16 +9,36 @@ let mobileMenu = document.querySelector('.menu');
 let body = document.querySelector('.body');
 
 hamburgerBtn.addEventListener('click', () => {
-    hamburgerBar1.classList.toggle('clicked');
-    hamburgerBar2.classList.toggle('clicked');
-    hamburgerBar3.classList.toggle('clicked');
-    mobileMenu.classList.toggle('menu--active');
-    body.classList.toggle('body--fixed');
+  hamburgerBar1.classList.toggle('clicked');
+  hamburgerBar2.classList.toggle('clicked');
+  hamburgerBar3.classList.toggle('clicked');
+  mobileMenu.classList.toggle('menu--active');
+  body.classList.toggle('body--fixed');
 });
 
+/*** ANIMATIONS ***/
+console.log('script loaded')
+/* HERO BLOCK */
 window.addEventListener("load", () => {
-  const elements = document.querySelectorAll(".hero .animate");
+  const elements = document.querySelectorAll(".animate");
   elements.forEach((el, i) => {
-    setTimeout(() => el.classList.add("show"), i * 800); // 400мс задержки между элементами
+    setTimeout(() => el.classList.add("show"), i * 800);
   });
 });
+
+/* OTHER SECTIONS */
+const observer = new IntersectionObserver((entries, obs) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("display");
+      /* obs.unobserve(entry.target); */ // анимация сработает один раз
+    }
+  });
+}, {
+  threshold: 0.9,
+  /* rootMargin: '0px 0px -100px 0px' */
+});
+
+// Наблюдаем все элементы animate-in на странице
+document.querySelectorAll(".animate-in").forEach(el => observer.observe(el));
+
